bd057a72d866f905487eaec106fb259ecda1a492
Sreevatsava1 <vatsava.gvr@gmail.com>
2020-10-17 17:17:32 -0400

    htmlrewriter done

--- worker/index.js
+++ worker/index.js
@@ -3,7 +3,27 @@ const links = [
                 {name: 'Link2', url: ''},
                 {name: 'Link3', url: ''}
               ]
+const image_src = '', username = 'vatsav'
 
+class LinksTransformer {
+  constructor(links) {
+    this.links = links
+  }
+  
+  async element(element) {
+    
+    links.forEach(link => {
+      element.append(`<a href=\"${link.url}\">${link.name}</a>`, {
+        html : true
+      } )
+    });
+  }
+}
+
+const transformer = new HTMLRewriter().on('div#links', new LinksTransformer(links))
+                                      .on('div#profile', {element: (element) => element.removeAttribute('style')} )
+                                      .on('img#avatar', { element : (element) => element.setAttribute('src', image_src)})
+                                      .on('h1#name', { element : (element) => element.setInnerContent(username)})
 
 
 addEventListener('fetch', event => {
@@ -18,5 +38,9 @@ async function handleRequest(request) {
   const path = new URL(request.url).pathname
   if(path == '/links')  {
     return new Response(JSON.stringify(links), {headers:{'content-type':'application/json'}})
+  }
+  else {
+    const static_page = await fetch("https://static-links-page.signalnerve.workers.dev/")
+    return transformer.transform(static_page)
   }
 }
